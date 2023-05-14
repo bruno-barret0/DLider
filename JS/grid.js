a6504a4e63ec5320ace5cddafac671b62d7ea767
LeonardoManique <110477986+LeonardoManique@users.noreply.github.com>
2023-05-14 04:32:15 -0300

    .

--- JS/grid.js
+++ JS/grid.js
@@ -1,137 +1,61 @@
-// Array com dados dos produtos
-const products = [
-    {
-      name: 'Heiniken1',
-      price: 9.99,
-      image: '/images/breja.png'
-    },
-    {
-        name: 'Heiniken1',
-        price: 9.99,
-        image: '/images/breja.png'
-      },
-      {
-        name: 'Heiniken1',
-        price: 9.99,
-        image: '/images/breja.png'
-      },
-      {
-        name: 'Heiniken1',
-        price: 9.99,
-        image: '/images/breja.png'
-      },
-      {
-        name: 'Heiniken1',
-        price: 9.99,
-        image: '/images/breja.png'
-      },
-      {
-        name: 'Heiniken1',
-        price: 9.99,
-        image: '/images/breja.png'
-      },
-      {
-        name: 'Heiniken1',
-        price: 9.99,
-        image: '/images/breja.png'
-      },
-      {
-        name: 'Heiniken1',
-        price: 9.99,
-        image: '/images/breja.png'
-      },
-      {
-        name: 'Heiniken1',
-        price: 9.99,
-        image: '/images/breja.png'
-      },
-      {
-        name: 'Heiniken1',
-        price: 9.99,
-        image: '/images/breja.png'
-      },
-      {
-        name: 'Heiniken1',
-        price: 9.99,
-        image: '/images/breja.png'
-      },
-      {
-        name: 'Heiniken1',
-        price: 9.99,
-        image: '/images/breja.png'
-      },
-      {
-        name: 'Heiniken1',
-        price: 9.99,
-        image: '/images/breja.png'
-      },
-      {
-        name: 'Heiniken1',
-        price: 9.99,
-        image: '/images/breja.png'
-      },
-      {
-        name: 'Heiniken1',
-        price: 9.99,
-        image: '/images/breja.png'
-      },
-      {
-        name: 'Heiniken1',
-        price: 9.99,
-        image: '/images/breja.png'
-      },
-      {
-        name: 'Heiniken1',
-        price: 9.99,
-        image: '/images/breja.png'
-      },
-      {
-        name: 'Heiniken1',
-        price: 9.99,
-        image: '/images/breja.png'
-      },
-      {
-        name: 'Heiniken1',
-        price: 9.99,
-        image: '/images/breja.png'
-      },
-      {
-        name: 'Heiniken1',
-        price: 9.99,
-        image: '/images/breja.png'
-      },
-      {
-        name: 'Product 1',
-        price: 9.99,
-        image: '/images/breja.png'
-      },
-    
+const products = [];
 
-  ];
+function addProduct(name, price, image) {
+  const newProduct = {
+    name,
+    price,
+    image
+  };
+  products.push(newProduct);
+}
+addProduct('Heiniken1', 9.99, '/images/breja.png');
+addProduct('Product 1', 9.99, '/images/breja.png');
+addProduct('Product 2', 8.99, '/images/breja.png');
+addProduct('Product 2', 8.99, '/images/breja.png');
+addProduct('Product 2', 8.99, '/images/breja.png');
+addProduct('Product 2', 8.99, '/images/breja.png');
+addProduct('Product 2', 8.99, '/images/breja.png');
+addProduct('Product 2', 8.99, '/images/breja.png');
+addProduct('Product 2', 8.99, '/images/breja.png');
+addProduct('Product 2', 8.99, '/images/breja.png');
+addProduct('Product 2', 8.99, '/images/breja.png');
+addProduct('Product 2', 8.99, '/images/breja.png');
+addProduct('Heiniken1', 9.99, '/images/breja.png');
+addProduct('Product 1', 9.99, '/images/breja.png');
+addProduct('Product 2', 8.99, '/images/breja.png');
+addProduct('Product 2', 8.99, '/images/breja.png');
+addProduct('Product 2', 8.99, '/images/breja.png');
+addProduct('Product 2', 8.99, '/images/breja.png');
+addProduct('Product 2', 8.99, '/images/breja.png');
+addProduct('Product 2', 8.99, '/images/breja.png');
+addProduct('Product 2', 8.99, '/images/breja.png');
+addProduct('Product 2', 8.99, '/images/breja.png');
+addProduct('Product 2', 8.99, '/images/breja.png');
+addProduct('Product 2', 8.99, '/images/breja.png');
+
   
-  // Seleciona o elemento do DOM onde os produtos serão exibidos
-  const productGrid = document.querySelector('.product-grid');
+
+const productGrid = document.querySelector('.product-grid');
+
+products.forEach(product => {
+const productItem = document.createElement('div');
+productItem.classList.add('product-item');
   
-  // Loop pelos produtos e cria um elemento HTML para cada um
-  products.forEach(product => {
-    const productItem = document.createElement('div');
-    productItem.classList.add('product-item');
+const productImage = document.createElement('img');
+productImage.setAttribute('src', product.image);
+productImage.setAttribute('alt', product.name);
+productItem.appendChild(productImage);
   
-    const productImage = document.createElement('img');
-    productImage.setAttribute('src', product.image);
-    productImage.setAttribute('alt', product.name);
-    productItem.appendChild(productImage);
+const productName = document.createElement('h3');
+productName.textContent = product.name;
+productItem.appendChild(productName);
+productName.classList.add('name');
   
-    const productName = document.createElement('h3');
-    productName.textContent = product.name;
-    productItem.appendChild(productName);
-    productName.classList.add('name');
+const productPrice = document.createElement('span');
+productPrice.classList.add('price');
+productPrice.textContent = `$${product.price.toFixed(2)}`;
+productItem.appendChild(productPrice);  
   
-    const productPrice = document.createElement('span');
-    productPrice.classList.add('price');
-    productPrice.textContent = `$${product.price.toFixed(2)}`;
-    productItem.appendChild(productPrice);  
-  
-    productGrid.appendChild(productItem);
-  });
+productGrid.appendChild(productItem);
+});
   
